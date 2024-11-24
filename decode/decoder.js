0e11245b035b78fa94ba358b8b419e578a4d0ba2
stanley ike <stanleyikeh15@gmail.com>
2024-11-24 14:15:57 +0100

    commit all folders

---
+++ decode/decoder.js
@@ -0,0 +1,21 @@
+
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+
+const reverse = (alpha)=>{
+    rIndex = 25 - alphabet.indexOf(alpha)
+    return alphabet[rIndex]
+}
+function reverseWord(word){
+    wordOutput=""
+    for (const element of word) {
+       if(alphabet.includes(element)){
+         wordOutput+=reverse(element)
+       } 
+       else wordOutput += element
+        
+    }
+    return wordOutput;
+}
+
+console.log(reverseWord("how are you"))
